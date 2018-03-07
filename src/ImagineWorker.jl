60e3bee053f3231f69f67c6a11089d1b1f3ef59a
Cody-G <codyjgreer@gmail.com>
2018-03-07 15:01:56 -0600

    comment out code that requires hardware so that package can build for testing

--- src/ImagineWorker.jl
+++ src/ImagineWorker.jl
@@ -3,15 +3,15 @@ __precompile__(true)
 module ImagineWorker
 
 using ImagineInterface, Unitful
-using NIDAQ
+#using NIDAQ
 
 global DEVICE_PREFIX = ""        
 
-export _set_device
+#export _set_device
 
-_set_device{T<:AbstractString}(dev::T) = global DEVICE_PREFIX = String(dev)
+#_set_device{T<:AbstractString}(dev::T) = global DEVICE_PREFIX = String(dev)
 
-include("inputs.jl")
-include("outputs.jl")
+#include("inputs.jl")
+#include("outputs.jl")
 
 end #module
