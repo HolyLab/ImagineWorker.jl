5cd2f588c66cb684124d9993fe2339e2424ee555
Cody-G <codyjgreer@gmail.com>
2018-03-10 13:59:47 -0600

    Allow module to load without NIDAQ (It's not functional in that case)

--- src/ImagineWorker.jl
+++ src/ImagineWorker.jl
@@ -1,17 +1,19 @@
-__precompile__(true)
+__precompile__(false)
 
 module ImagineWorker
 
 using ImagineInterface, Unitful
-using NIDAQ
 
-global DEVICE_PREFIX = ""        
+if is_windows() && isfile("C:\\Windows\\System32\\nicaiu.dll")
+    using NIDAQ
+    global DEVICE_PREFIX = ""        
 
-export _set_device
+    export _set_device
 
-_set_device{T<:AbstractString}(dev::T) = global DEVICE_PREFIX = String(dev)
+    _set_device{T<:AbstractString}(dev::T) = global DEVICE_PREFIX = String(dev)
 
-include("inputs.jl")
-include("outputs.jl")
+    include("inputs.jl")
+    include("outputs.jl")
+end
 
 end #module
